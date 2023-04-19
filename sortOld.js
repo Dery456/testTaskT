ff33a45426e5db26770f1da3943483ad0c270f32
Dery456 <n_krosh@mail.ru>
2023-04-19 15:23:32 +0300

    6 task

--- sortOld.js
+++ sortOld.js
@@ -1,5 +1,8 @@
 const sortByAge = (user) => {
-  return user["age"].sort((a, b) => a - b);
+  if (user) {
+    return user.sort((a, b) => a.age - b.age);
+  }
+  return 0;
 };
 
 let vasya = { name: "Вася", age: 25 };
@@ -8,4 +11,34 @@ let masha = { name: "Маша", age: 28 };
 
 let arr = [vasya, petya, masha];
 
-console.log(sortByAge(arr));
+sortByAge(arr);
+
+console.log(arr[0].name === "Вася");
+console.log(arr[1].name === "Маша");
+console.log(arr[2].name === "Петя");
+
+let vasya1 = { name: "Вася", age: 3 };
+let petya1 = { name: "Петя", age: 2 };
+let masha1 = { name: "Маша", age: 1 };
+
+let arr1 = [vasya1, petya1, masha1];
+
+sortByAge(arr1);
+
+console.log(arr1[0].name === "Маша");
+console.log(arr1[1].name === "Петя");
+console.log(arr1[2].name === "Вася");
+
+let vasya2 = { name: "Вася", age: 0 };
+let petya2 = { name: "Петя", age: 0 };
+let masha2 = { name: "Маша", age: 1 };
+
+let arr2 = [vasya2, petya2, masha2];
+
+sortByAge(arr2);
+
+console.log(arr2[0].name === "Вася");
+console.log(arr2[1].name === "Петя");
+console.log(arr2[2].name === "Маша");
+
+console.log(sortByAge() === 0);
